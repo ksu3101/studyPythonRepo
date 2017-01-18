7fb1f889215c59bbc9127c5987450872ba6e5cc7
burkdog <burkdog@naver.com>
2017-01-18 22:54:18 +0900

    update contents

---
+++ studyList.py
@@ -0,0 +1,19 @@
+# List study
+
+# list examples
+list1 = []
+list2 = [1, 2, 3]
+list3 = ['kang', 'kim', 'park']
+list4 = ['kim', 100, 0.15, 'lee']
+list5 = [1, ['kim', 'jung', 'lee'], 2, 3]
+
+strList = ['kim', 'lee', 'park', 'kang']
+print(strList[0] + strList[2])
+
+print(strList[-1])
+testList = [1, 4, 6, 12, 20]
+print(testList[-1])
+testList2 = [5, 6, 7, ['kang', 'lee']]
+print(testList2[-1])
+
+print(testList2[-1][1])
