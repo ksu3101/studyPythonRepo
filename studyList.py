d009f8aba17a1a7b056c5ebc65096261635ddc4f
burkdog <burkdog@naver.com>
2017-01-20 23:45:46 +0900

    update contents

--- studyList.py
+++ studyList.py
@@ -17,3 +17,59 @@ testList2 = [5, 6, 7, ['kang', 'lee']]
 print(testList2[-1])
 
 print(testList2[-1][1])
+
+
+aList = [1, 2, 3, 4]
+bList = [5, 6, 7, 8]
+
+# 리스트 더하기
+print(aList + bList)
+
+# 리스트 반복
+print(aList * 2)
+print(bList * 3)
+
+# 리스트에서 원소 수정 하기
+elist = [3, 5, 6]
+elist[1] = 100
+print(elist)
+
+# 리스트에서 연속적 원소들을 수정 하기
+elist = [4, 5, 6, 7]
+elist[1:2] = ['a', 'b']
+print(elist)
+
+elist = [4, 5, 6, 7]
+elist[1] = ['a', 'b']
+print(elist)
+
+# 리스트에서 원소를 삭제 하기
+elist = [4, 5, 6, 7, 8, 9]
+elist[1:3] = []
+print(elist)
+
+# 리스트에서 원소를 삭제
+elist = [4, 5, 6, 7]
+del elist[1]
+print(elist)
+
+# 리스트의 정렬
+numList = [4, 2, 12, 8, 5, 1, 3]
+numList.sort()
+print(numList)
+
+textList = ['r', 'Kim', 'jung', 'B', 'c', 'b', 'Zed', 'a']
+textList.sort()
+print(textList)
+
+# 리스트 뒤집기
+elist = [1, 2, 3, 4]
+elist.reverse()
+print(elist)
+
+# 원소 탐색 하고 인덱스 얻기
+elist = [11, 22, 33, 44]
+print(elist.index(22))
+slist = ['abc', 'kim', 'lee', 'park']
+print(slist.index('abc'))
+print(slist.index('zzzz'))      # 없을 경우
