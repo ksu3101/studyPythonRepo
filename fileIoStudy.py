c5018d3eeb8213d744a674f2715ded98352071dd
burkdog <burkdog@naver.com>
2017-02-05 14:19:52 +0900

    update contents

--- fileIoStudy.py
+++ fileIoStudy.py
@@ -2,12 +2,55 @@
 
 import sys
 
+filePath = "./study/fileiostudy.txt"
+
 # open("파일이름", "모드")
-f = open('./study/fileiostudy.txt', 'w')
+f = open(filePath, 'w')
 
 f.write("Hello world\n")
 for i in range(10):
-	f.write("%d " % i)
+	f.write("line : %d \n" % i)
 
 f.close()
 
+# - - - -
+
+f = open(filePath, 'a')
+for i in range(500, 510):
+	f.write("line : %d \n" % i)
+f.close()
+
+# - - - -
+
+with open(filePath, 'a') as f:
+	f.write("Monty python's Flying Circus")
+
+# - - - -
+
+f = open(filePath, 'r')
+
+reads = f.read()
+print(reads)
+
+f.close()
+
+
+"""
+readLines = f.readlines()
+for line in readLines:
+	print(line)
+
+f.close()
+"""
+
+
+"""
+f = open(filePath, 'r')
+
+while True:
+	readLine = f.readline()
+	if not readLine: break;
+	print(readLine)
+
+f.close()
+"""
--- playground.py
+++ playground.py
@@ -1,5 +1,13 @@
 # play ground py
 
+result = ""
+while True:
+	lineInput = input()
+	if not lineInput: break
+	result += lineInput
+print(result)
+
+"""
 print("안녕", "하세요.", "반갑 습니다.", "좋은 하루 되세요")
 
 for i in range(5):
@@ -13,3 +21,4 @@ print()
 users = {"kim", "jung", "park", "lee"}
 for user in users:
 	print(user, end=", ")
+"""
