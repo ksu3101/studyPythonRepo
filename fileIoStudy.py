11d19e01c18968f6350db82121ac490c8a92e595
burkdog <burkdog@naver.com>
2017-02-05 11:57:45 +0900

    update contents

--- fileIoStudy.py
+++ fileIoStudy.py
@@ -3,6 +3,11 @@
 import sys
 
 # open("파일이름", "모드")
-f = open('fileiostudy.txt', 'w')
+f = open('./study/fileiostudy.txt', 'w')
+
+f.write("Hello world\n")
+for i in range(10):
+	f.write("%d " % i)
+
 f.close()
 
