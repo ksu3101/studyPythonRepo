fae9b09c42ad682bfcb4106c12d265b9bcf3c3b2
burkdog <burkdog@naver.com>
2017-02-14 21:40:47 +0900

    update contents

---
+++ calc.py
@@ -0,0 +1,14 @@
+# module calc.py
+
+def sum(x, y):
+	return x + y
+
+def minus(x, y):
+	return x - y
+
+def divide(x, y):
+	if x == 0 or y == 0: return 0
+	return x / y
+
+def multiply(x, y):
+	return x * y
---
+++ main_modulestudy.py
@@ -0,0 +1,5 @@
+
+import calc
+
+print(calc.sum(10, 30))
+print(calc.multiply(4, 8))
